733800e6a4e52caf6c865cc4ce56ddb21b490796
Pedro <pedroasso@hotmail.com>
2025-01-14 22:46:32 -0500

    [api]: finish api connection

--- src/app/utils/send-email.ts
+++ src/app/utils/send-email.ts
@@ -1,7 +1,21 @@
 import { FormData } from "@components/ContactForm";
 
-export const sendEmail = (data: FormData) => {
-    return (
-        console.log(data)
-    )
+export function sendEmail (data: FormData) {
+
+    const apiEndpoint = '/api/email';
+
+    fetch(apiEndpoint, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(data),
+    })
+    .then((res) => res.json())
+    .then((response) => {
+        alert(response.message);
+    })
+    .catch((err) => {
+        alert(err);
+    })
 }
