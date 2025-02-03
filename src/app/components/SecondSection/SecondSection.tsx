289f7d48d1804aa3df252ea67c1080bbd2378448
Pedro <pedroasso@hotmail.com>
2025-02-02 23:32:40 -0500

    [projects]: add project number 2

--- src/app/components/SecondSection/SecondSection.tsx
+++ src/app/components/SecondSection/SecondSection.tsx
@@ -1,7 +1,7 @@
 import Image from "next/image";
 import { FaArrowRight } from "react-icons/fa";
 import ProjectImageOne from "../../../../public/to-do-list-thumbnail.webp";
-import ProjectImageTwo from "../../../../public/logo.png";
+import ProjectImageTwo from "../../../../public/weather-app-thumbnail.png";
 import ProjectImageThree from "../../../../public/logo.png";
 import ProjectImageFour from "../../../../public/logo.png";
 import ProjectImageFive from "../../../../public/logo.png";
@@ -60,15 +60,19 @@ export const SecondSection = () => {
             />
           </div>
           <article className="text-[#F0F4F8] lg:p-2 lg:max-w-[70%]">
-            <h4 className="font-bold text-xl 2xl:text-2xl">PROJECT NAME</h4>
+            <h4 className="font-bold text-xl 2xl:text-2xl">WEATHER APP</h4>
             <p className="pt-6 2xl:w-9/12 xl:text-xl 2xl:text-2xl">
-              Lorem Ipsum is simply dummy text of the printing and typesetting
-              industry. Lorem Ipsum has been the industry's standard dummy text
-              ever since the 1500s, when an unknown printer took a galley of
-              type and scrambled it to make a type specimen book.
+              SkyWatch is a weather forecast application built with Next.js and
+              Tailwind CSS, utilizing the WeatherAPI.com to provide real-time
+              weather data. It allows users to search for weather conditions by
+              city, address, or postal code, displaying current temperature,
+              wind speed, humidity, sunrise and sunset times, and a 10-day
+              forecast. With a clean and responsive interface, SkyWatch offers a
+              seamless experience for checking weather updates quickly and
+              efficiently. 🚀🌤️
             </p>
             <div className="py-2 xl:py-4 flex flex-row flex-nowrap justify-between items-center md:w-1/2 lg:w-4/5 xl:w-3/5 xl:text-xl">
-              <a href="https://github.com/laranjeirapedro/portfolio_project--to-do-list">
+              <a href="https://github.com/laranjeirapedro/portfolio_project--weather-app.git">
                 <button className="w-20 md:w-28 p-2 border-solid border-[#F0F4F8] border-2 rounded-md bg-[#A0D6FF] text-[#333333] font-bold">
                   Git Hub
                 </button>
