903dda189477d880c85c0b786a5e997363405446
Pedro <pedroasso@hotmail.com>
2025-02-05 20:43:55 -0500

    [analytics]: add google tag

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,41 @@
 import React from "react";
-import {LayoutBase, FirstSection, SecondSection, ThirdSection} from "./components";
+import Script from "next/script";
+import {
+  LayoutBase,
+  FirstSection,
+  SecondSection,
+  ThirdSection,
+} from "./components";
 
 const Home = () => {
   return (
-    <LayoutBase>
-      <FirstSection />
-      <SecondSection />
-      <ThirdSection />
-    </LayoutBase>
+    <>
+      {/* Google Analytics */}
+      <Script
+        strategy="afterInteractive"
+        src={`https://www.googletagmanager.com/gtag/js?id=G-3ED7BXVBN1`}
+      />
+      <Script
+        id="google-analytics"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-3ED7BXVBN1', {
+              page_path: window.location.pathname,
+            });
+          `,
+        }}
+      />
+      <LayoutBase>
+        <FirstSection />
+        <SecondSection />
+        <ThirdSection />
+      </LayoutBase>
+    </>
   );
-}
+};
 
 export default Home;
