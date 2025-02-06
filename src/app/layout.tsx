3f19209b2292ed89814fc086488e7ccf1c9dc6d9
Pedro <pedroasso@hotmail.com>
2025-02-05 20:50:45 -0500

    [analytics]: fix google tag

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from "next";
+import Script from "next/script";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./styles/globals.css";
 
@@ -24,6 +25,27 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        {/* Google Analytics */}
+        <Script
+          strategy="afterInteractive"
+          src={`https://www.googletagmanager.com/gtag/js?id=G-3ED7BXVBN1`}
+        />
+        <Script
+          id="google-analytics"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-3ED7BXVBN1', {
+              page_path: window.location.pathname,
+            });
+          `,
+          }}
+        />
+      </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import Script from "next/script";
+
 import {
   LayoutBase,
   FirstSection,
@@ -9,32 +9,11 @@ import {
 
 const Home = () => {
   return (
-    <>
-      {/* Google Analytics */}
-      <Script
-        strategy="afterInteractive"
-        src={`https://www.googletagmanager.com/gtag/js?id=G-3ED7BXVBN1`}
-      />
-      <Script
-        id="google-analytics"
-        strategy="afterInteractive"
-        dangerouslySetInnerHTML={{
-          __html: `
-            window.dataLayer = window.dataLayer || [];
-            function gtag(){dataLayer.push(arguments);}
-            gtag('js', new Date());
-            gtag('config', 'G-3ED7BXVBN1', {
-              page_path: window.location.pathname,
-            });
-          `,
-        }}
-      />
       <LayoutBase>
         <FirstSection />
         <SecondSection />
         <ThirdSection />
       </LayoutBase>
-    </>
   );
 };
 
